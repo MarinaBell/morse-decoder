433694df60c83fc6ea76c23022b78fe912b9753d
Marina Belenkaya <marina_vorobyeva1423@mail.ru>
2022-03-03 11:38:55 +0400

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+    function decode(expr) {
+      let arr2 = [];
+      let arr3 = [];
+      let arr = expr
+      .replace(/\*{10}/g, ' ,') // звёздочки заменяет их на пробел, после него ставится запятая
+      .replace(/(\d{10})/g, '$1,') // строка раздляется запятыми на блоки по 10 цифр
+      .split(',') // преобразование в массив
+    
+      for (let i = 0; i < arr.length; i++) {
+        arr2[i] = (arr[i].replace(/^0+/, ''))
+        
+        arr3[i] = arr2[i].replace(/(\d{2})/g, (subchar) => {
+          switch (subchar) {
+            case '00': return '';
+            case '10' : return '.';
+            case '11' : return '-';
+            default: return;
+          }
+        });
+      }
+      char = arr3.join(''); // перевод массива в строку
+      result =  (arr3.map(char => MORSE_TABLE[char] || ' '). join(''));
+      return result.trimEnd();
+      }
 
 module.exports = {
     decode
+    
 }
